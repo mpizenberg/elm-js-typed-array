17494b412cd507d2f8531ae725757fa434b183bc
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2017-12-06 15:45:55 +0800

    Use clang-format

--- src/Native/JsArrayBuffer.js
+++ src/Native/JsArrayBuffer.js
@@ -1,21 +1,17 @@
 var _mpizenberg$elm_js_typed_array$Native_JsArrayBuffer = function() {
 
-function initialize( length ) {
-	return new ArrayBuffer( length );
+  function initialize(length) { return new ArrayBuffer(length); }
+
+  function length(arrayBuffer) { return arrayBuffer.byteLength; }
+
+  function slice(begin, end, arrayBuffer) {
+    return arrayBuffer.slice(begin, end);
+  }
+
+  return {
+    initialize : initialize,
+    length : length,
+    slice : F3(slice),
+  };
 }
-
-function length( arrayBuffer ) {
-	return arrayBuffer.byteLength;
-}
-
-function slice( begin, end, arrayBuffer ) {
-	return arrayBuffer.slice( begin, end );
-}
-
-return {
-	initialize: initialize,
-	length: length,
-	slice: F3(slice),
-};
-
-}();
+();
--- src/Native/JsTypedArray.js
+++ src/Native/JsTypedArray.js
@@ -1,33 +1,27 @@
 var _mpizenberg$elm_js_typed_array$Native_JsTypedArray = function() {
 
-function length( typedArray ) {
-	return typedArray.length;
+  function length(typedArray) { return typedArray.length; }
+
+  function buffer(typedArray) { return typedArray.buffer; }
+
+  function bufferOffset(typedArray) { return typedArray.byteOffset; }
+
+  function all(f, typedArray) {
+    function flippedF(element, index) { return A2(f, index, element); }
+    return typedArray.every(flippedF);
+  }
+
+  function any(f, typedArray) {
+    function flippedF(element, index) { return A2(f, index, element); }
+    return typedArray.some(flippedF);
+  }
+
+  return {
+    length : length,
+    buffer : buffer,
+    bufferOffset : bufferOffset,
+    all : F2(all),
+    any : F2(any),
+  };
 }
-
-function buffer( typedArray ) {
-	return typedArray.buffer;
-}
-
-function bufferOffset( typedArray ) {
-	return typedArray.byteOffset;
-}
-
-function all( f, typedArray ) {
-	function flippedF( element, index ) { return A2( f, index, element ); }
-	return typedArray.every( flippedF );
-}
-
-function any( f, typedArray ) {
-	function flippedF( element, index ) { return A2( f, index, element ); }
-	return typedArray.some( flippedF );
-}
-
-return {
-	length: length,
-	buffer: buffer,
-	bufferOffset: bufferOffset,
-	all: F2( all ),
-	any: F2( any ),
-};
-
-}();
+();
