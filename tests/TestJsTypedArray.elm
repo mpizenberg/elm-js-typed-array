a096c5fc14d5d7106771c1d07d413113ece5a6b3
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2017-12-07 14:56:39 +0800

    Test indexedMap and extract

--- tests/TestJsTypedArray.elm
+++ tests/TestJsTypedArray.elm
@@ -1,7 +1,9 @@
 module TestJsTypedArray
     exposing
-        ( indexedAll
+        ( extract
+        , indexedAll
         , indexedAny
+        , indexedMap
         )
 
 import Expect exposing (Expectation)
@@ -14,6 +16,14 @@ import Test exposing (..)
 lengthFuzzer : Fuzzer Int
 lengthFuzzer =
     Fuzz.intRange 0 1000
+
+
+arrayIndex : Int -> Int -> Int
+arrayIndex length idx =
+    if idx < 0 then
+        max 0 (length - idx)
+    else
+        idx
 
 
 indexedAll : Test
@@ -61,4 +71,55 @@ indexedAny =
                         |> Expect.true "Returns False if predicate returns False one time"
                 else
                     Expect.pass
+        ]
+
+
+indexedMap : Test
+indexedMap =
+    describe "indexedMap"
+        [ fuzz lengthFuzzer "indexedMap preserve length" <|
+            \length ->
+                JsUint8Array.initialize length
+                    |> JsTypedArray.indexedMap (\_ _ -> 42)
+                    |> JsTypedArray.length
+                    |> Expect.equal length
+        , fuzz lengthFuzzer "indexedMap coherent" <|
+            \length ->
+                JsUint8Array.initialize length
+                    |> JsTypedArray.indexedMap (\id _ -> id)
+                    |> JsTypedArray.indexedAll (\id value -> id % 256 == value)
+                    |> Expect.true "All values set to index"
+        ]
+
+
+extract : Test
+extract =
+    describe "extract"
+        [ fuzz3 lengthFuzzer lengthFuzzer lengthFuzzer "Extract with correct indices" <|
+            \a b c ->
+                case List.sort [ a, b, c ] of
+                    l1 :: l2 :: l3 :: [] ->
+                        JsUint8Array.initialize l3
+                            |> JsTypedArray.indexedMap (\id _ -> id)
+                            |> JsTypedArray.extract l1 l2
+                            |> JsTypedArray.indexedAll (\id value -> value == (id + l1) % 256)
+                            |> Expect.true "Values correspond to index extracted"
+
+                    _ ->
+                        Expect.fail "This branch is never called"
+        , fuzz3 lengthFuzzer lengthFuzzer lengthFuzzer "Extract with any indices" <|
+            \length start end ->
+                let
+                    typedArray =
+                        JsUint8Array.initialize length
+                            |> JsTypedArray.indexedMap (\id _ -> id)
+
+                    correctStart =
+                        arrayIndex length start
+
+                    correctEnd =
+                        arrayIndex length end
+                in
+                JsTypedArray.extract start end typedArray
+                    |> Expect.equal (JsTypedArray.extract correctStart correctEnd typedArray)
         ]
