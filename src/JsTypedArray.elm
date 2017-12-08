9391498a32fb04ebed55925d5d557f2af8b55496
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2017-12-08 18:23:31 +0800

    Improve documentation

--- src/JsTypedArray.elm
+++ src/JsTypedArray.elm
@@ -25,11 +25,11 @@ module JsTypedArray
         , sort
         )
 
-{-| This module wraps Javascript typed arrays in elm.
+{-| This module wraps JavaScript typed arrays in elm.
 
 @docs JsTypedArray
 
-The list of all JS typed arrays representable is as below:
+The list of all JavaScript typed arrays representable is as below:
 
 @docs Uint8, Float64
 
@@ -37,7 +37,7 @@ The list of all JS typed arrays representable is as below:
 # Typed Array Creation
 
 Functions to initialize typed arrays are in their dedicated modules.
-See for example the function `JsUint8Array.init : Int -> JsTypedArray Uint8 Int`
+See for example the function `JsUint8Array.initialize : Int -> JsTypedArray Uint8 Int`
 in the `JsUint8Array` module.
 
 
@@ -82,7 +82,7 @@ import JsArrayBuffer exposing (JsArrayBuffer)
 import Native.JsTypedArray
 
 
-{-| `JsTypedArray a b` represents a [Javascript typed array][typed-array].
+{-| `JsTypedArray a b` represents a [JavaScript typed array][typed-array].
 
 [typed-array]: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Typed_arrays
 
@@ -93,7 +93,7 @@ The second type parameter `b` is used to indicate which elm type is compatible
 with the data type in the array. Most probably, it will be `Int` or `Float`.
 
 Those type parameters (`a` and `b`) are fixed at the creation of the array.
-For example, a function generating a Javascript `Uint8Array` will have the return type:
+For example, a function generating a JavaScript `Uint8Array` will have the return type:
 `JsTypedArray Uint8 Int`.
 
 -}
@@ -118,11 +118,15 @@ type Float64
 
 
 {-| Get the number of elements in the array.
-
 Internally uses `TypedArray.prototype.length`.
-Beware that this length is different from its buffer length.
+Beware that this length (in number of elements)
+is different from its buffer length (in bytes).
 
 Complexity: O(1).
+
+    JsUint8Array.initialize 5
+        |> JsTypedArray.length
+    --> 5
 
 -}
 length : JsTypedArray a b -> Int
@@ -131,10 +135,17 @@ length =
 
 
 {-| Get the value at given index.
-
 Return `Nothing` if index is outside of bounds.
 
 Complexity: O(1).
+
+    JsUint8Array.fromList [0, 14, 42]
+        |> JsTypedArray.getAt 2
+    --> Just 42
+
+    JsUint8Array.fromList [0, 14, 42]
+        |> JsTypedArray.getAt 3
+    --> Nothing
 
 -}
 getAt : Int -> JsTypedArray a b -> Maybe b
@@ -146,10 +157,19 @@ getAt index array =
 
 
 {-| Get the underlying data buffer of the array.
-
 Internally uses `TypedArray.prototype.buffer`.
 
 Complexity: O(1).
+
+    JsUint8Array.fromList [0, 14, 42]
+        |> JsTypedArray.buffer
+        |> JsArrayBuffer.length
+    --> 3
+
+    JsFloat64Array.fromList [0, 14, 42]
+        |> JsTypedArray.buffer
+        |> JsArrayBuffer.length
+    --> 24
 
 -}
 buffer : JsTypedArray a b -> JsArrayBuffer
@@ -158,10 +178,14 @@ buffer =
 
 
 {-| Get the offset (in bytes) from the start of its corresponding buffer.
-
 Internally uses `TypedArray.prototype.byteOffset`.
 
 Complexity: O(1).
+
+    JsArrayBuffer.initialize 5
+        |> JsUint8Array.fromBuffer 3 2
+        |> Result.map JsTypedArray.bufferOffset
+    --> Ok 3
 
 -}
 bufferOffset : JsTypedArray a b -> Int
@@ -174,10 +198,17 @@ bufferOffset =
 
 
 {-| Return `True` if all elements satisfy the predicate.
-
 Internally uses `TypedArray.prototype.every`.
 
 Complexity: O(length).
+
+    JsUint8Array.fromList [0, 14, 42]
+        |> JsTypedArray.indexedAll (\_ v -> v < 50)
+    --> True
+
+    JsUint8Array.fromList [0, 14, 42]
+        |> JsTypedArray.indexedAll (\_ v -> v < 20)
+    --> False
 
 -}
 indexedAll : (Int -> b -> Bool) -> JsTypedArray a b -> Bool
@@ -186,10 +217,17 @@ indexedAll =
 
 
 {-| Return `True` if at least one element satisfies the predicate.
-
 Internally uses `TypedArray.prototype.some`.
 
 Complexity: O(length).
+
+    JsUint8Array.fromList [0, 14, 42]
+        |> JsTypedArray.indexedAny (\_ v -> v > 50)
+    --> False
+
+    JsUint8Array.fromList [0, 14, 42]
+        |> JsTypedArray.indexedAny (\_ v -> v > 20)
+    --> True
 
 -}
 indexedAny : (Int -> b -> Bool) -> JsTypedArray a b -> Bool
@@ -197,12 +235,19 @@ indexedAny =
     Native.JsTypedArray.indexedAny
 
 
-{-| Returns the index of the first element satisfying the predicate.
-
+{-| Return the index of the first element satisfying the predicate.
 If no element satisfies it, returns `Nothing`.
 Internally uses `TypedArray.prototype.findIndex`.
 
 Complexity: O(length).
+
+    JsUint8Array.fromList [0, 14, 42]
+        |> JsTypedArray.findIndex (\_ v -> v > 20)
+    --> Just 2
+
+    JsUint8Array.fromList [0, 14, 42]
+        |> JsTypedArray.findIndex (\_ v -> v > 50)
+    --> Nothing
 
 -}
 findIndex : (Int -> b -> Bool) -> JsTypedArray a b -> Maybe Int
@@ -211,10 +256,17 @@ findIndex =
 
 
 {-| Filter an array, keeping only elements satisfying the predicate.
-
 Internally uses `TypedArray.prototype.filter`.
 
 Complexity: O(length).
+
+    JsUint8Array.fromList [0, 14, 42]
+        |> JsTypedArray.indexedFilter (\_ v -> v > 20)
+    --> { 0 = 42 }
+
+    JsUint8Array.fromList [0, 14, 42]
+        |> JsTypedArray.indexedFilter (\_ v -> v < 20)
+    --> { 0 = 0, 1 = 14 }
 
 -}
 indexedFilter : (Int -> b -> Bool) -> JsTypedArray a b -> JsTypedArray a b
@@ -226,12 +278,31 @@ indexedFilter =
 -- ARRAY EXTRACTIONS #################################################
 
 
-{-| Extract a region of the array.
-
+{-| Extract a segment of the array between given start (included)
+and end (excluded) indices.
 Internally uses `TypedArray.prototype.subarray`
 which reuses the same buffer, changing offset and length attributes.
+Thus this is extremely fast since it doesn't copy the data buffer.
+
+In case of negative indices, count backward from end of array.
 
 Complexity: O(1).
+
+    f64Array =
+        JsFloat64Array.fromList [0, 14, 42]
+            |> JsTypedArray.extract 1 3
+    --> { 0 = 14, 1 = 42 }
+
+    JsTypedArray.bufferOffset f64Array
+    --> 8
+
+    JsFloat64Array.fromList [0, 14, 42]
+        |> JsTypedArray.extract -2 -1
+    --> { 0 = 14 }
+
+    JsFloat64Array.fromList [0, 14, 42]
+        |> JsTypedArray.extract -2 100000
+    --> { 0 = 14, 1 = 42 }
 
 -}
 extract : Int -> Int -> JsTypedArray a b -> JsTypedArray a b
@@ -243,11 +314,16 @@ extract =
 -- ARRAY TRANSFORMATIONS #############################################
 
 
-{-| Replace a segment of the array by a constant value.
-
+{-| Replace a segment [start, end[ of the array by a constant value.
 Internally uses `TypedArray.prototype.fill`.
 
+Negative indices are counted backward from end of array.
+
 Complexity: O(length).
+
+    JsUint8Array.fromList [0, 14, 42]
+        |> JsTypedArray.replaceWithConstant 1 3 17
+    --> { 0 = 0, 1 = 17, 2 = 17 }
 
 -}
 replaceWithConstant : Int -> Int -> b -> JsTypedArray a b -> JsTypedArray a b
@@ -256,10 +332,17 @@ replaceWithConstant =
 
 
 {-| Apply a function to every element of the array.
-
 Internally uses `TypedArray.prototype.map`.
 
 Complexity: O(length).
+
+    JsUint8Array.initialize 3
+        |> JsTypedArray.indexedMap (\id _ -> id)
+    --> { 0 = 0, 1 = 1, 2 = 2 }
+
+    JsUint8Array.fromList [0, 14, 42]
+        |> JsTypedArray.indexedMap (\_ v -> v + 1)
+    --> { 0 = 1, 1 = 15, 2 = 43 }
 
 -}
 indexedMap : (Int -> b -> b) -> JsTypedArray a b -> JsTypedArray a b
@@ -268,8 +351,24 @@ indexedMap =
 
 
 {-| Apply a function to every element of two arrays to form a new one.
+The bigger array is troncated at the size of the smaller one.
 
 Complexity: O(length).
+
+    array1 =
+        JsUint8Array.fromList [ 0, 1, 2 ]
+
+    array2 =
+        JsUint8Array.fromList [ 0, 14, 42 ]
+
+    array3 =
+        JsUint8Array.fromList [ 0, 1, 2, 3, 4, 5, 6, 7 ]
+
+    JsTypedArray.indexedMap2 (\_ x y -> x + y) array1 array2
+    --> { 0 = 0, 1 = 15, 2 = 44 }
+
+    JsTypedArray.indexedMap2 (\_ x y -> x + y) array1 array3
+    --> { 0 = 0, 1 = 2, 2 = 4 }
 
 -}
 indexedMap2 : (Int -> b -> b -> b) -> JsTypedArray a b -> JsTypedArray a b -> JsTypedArray a b
@@ -288,7 +387,6 @@ indexedMap2 f typedArray1 typedArray2 =
 
 
 {-| Reverse the array.
-
 Internally uses `TypedArray.prototype.reverse`.
 
 Complexity: O(length).
@@ -300,7 +398,6 @@ reverse =
 
 
 {-| Sort the array.
-
 Internally uses `TypedArray.prototype.sort`.
 
 Complexity: depends on browser implementation.
@@ -312,7 +409,6 @@ sort =
 
 
 {-| Sort the array in reverse order.
-
 Internally uses `TypedArray.prototype.sort`.
 
 Complexity: depends on browser implementation.
@@ -328,10 +424,13 @@ reverseSort =
 
 
 {-| Join array values in a string using the given separator.
-
 Internally uses `TypedArray.prototype.join`.
 
 Complexity: O(length).
+
+    JsUint8Array.fromList [0, 14, 42]
+        |> JsTypedArray.join ','
+    --> "0,14,42"
 
 -}
 join : Char -> JsTypedArray a b -> String
@@ -340,10 +439,17 @@ join =
 
 
 {-| Reduce the array from the left.
-
 Internally uses `TypedArray.prototype.reduce`.
 
 Complexity: O(length).
+
+    sumArray : JsTypedArray a number -> number
+    sumArray =
+        JsTypedArray.indexedFoldl (always (+)) 0
+
+    JsUint8Array.fromList [0, 14, 42]
+        |> sumArray
+    --> 56
 
 -}
 indexedFoldl : (Int -> b -> c -> c) -> c -> JsTypedArray a b -> c
@@ -352,10 +458,17 @@ indexedFoldl =
 
 
 {-| Reduce the array from the right.
-
 Internally uses `TypedArray.prototype.reduceRight`.
 
 Complexity: O(length).
+
+    arrayToList : JsTypedArray a b -> List b
+    arrayToList =
+        JsTypedArray.indexedFoldr (always (::)) []
+
+    JsUint8Array.fromList [0, 14, 42]
+        |> arrayToList
+    --> [0, 14, 42]
 
 -}
 indexedFoldr : (Int -> b -> c -> c) -> c -> JsTypedArray a b -> c
@@ -364,8 +477,22 @@ indexedFoldr =
 
 
 {-| Reduce two arrays from the left.
+The longer array is troncated at the size of the smaller one.
 
 Complexity: O(length).
+
+    innerProduct : JsTypedArray a number -> JsTypedArray a number -> number
+    innerProduct =
+        JsTypedArray.indexedFoldl2 (\_ x y product -> x * y + product) 0
+
+    array1 =
+        JsUint8Array.fromList [0, 1, 2]
+
+    array2 =
+        JsUint8Array.fromList [0, 14, 42, 10000]
+
+    innerProduct array1 array2
+    --> 98
 
 -}
 indexedFoldl2 : (Int -> b -> b -> c -> c) -> c -> JsTypedArray a b -> JsTypedArray a b -> c
@@ -384,8 +511,22 @@ indexedFoldl2 f initialValue typedArray1 typedArray2 =
 
 
 {-| Reduce two arrays from the right.
+The longer array is troncated at the size of the smaller one.
 
 Complexity: O(length).
+
+    toZipList : JsTypedArray a b -> JsTypedArray a b -> List (b,b)
+    toZipList =
+        JsTypedArray.indexedFoldr2 (\_ x y list -> (x,y) :: list) []
+
+    array1 =
+        JsUint8Array.fromList [0, 1, 2]
+
+    array2 =
+        JsUint8Array.fromList [0, 14, 42, 10000]
+
+    toZipList array1 array2
+    --> [(0,0),(1,14),(2,42)]
 
 -}
 indexedFoldr2 : (Int -> b -> b -> c -> c) -> c -> JsTypedArray a b -> JsTypedArray a b -> c
