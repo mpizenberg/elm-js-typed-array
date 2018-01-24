c82a839488263ecff4ea494d63cecc7255e26a7c
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2018-01-24 10:55:38 +0800

    Correct test bug due to NaN /= NaN in Float case

--- tests/TestJsDataView.elm
+++ tests/TestJsDataView.elm
@@ -6,7 +6,7 @@ module TestJsDataView
         , getterSetterRoundTrip
         )
 
-import Expect
+import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer)
 import JsArrayBuffer exposing (JsArrayBuffer)
 import JsDataView exposing (JsDataView)
@@ -77,15 +77,15 @@ type alias Setter a =
     Int -> a -> JsDataView -> JsDataView
 
 
-roundTrip : Getter number -> Setter number -> Int -> JsDataView -> Bool
+roundTrip : Getter number -> Setter number -> Int -> JsDataView -> ( number, number )
 roundTrip getter setter index dataView =
     getter index dataView
         |> Result.map (\val -> ( val, setter index val dataView ))
-        |> Result.map (\( val, view ) -> val == (Result.withDefault 0 <| getter index view))
-        |> Result.withDefault False
+        |> Result.map (\( val, newDataView ) -> ( val, Result.withDefault 0 <| getter index newDataView ))
+        |> Result.withDefault ( 0, 0 )
 
 
-validGetterSetterRoundTrip : Int -> Getter number -> Setter number -> JsDataView -> Bool
+validGetterSetterRoundTrip : Int -> Getter number -> Setter number -> JsDataView -> ( List number, List number )
 validGetterSetterRoundTrip elementSize getter setter dataView =
     let
         roundTripAt index =
@@ -99,7 +99,25 @@ validGetterSetterRoundTrip elementSize getter setter dataView =
     in
     List.range 0 (fittingNbElem - 1)
         |> List.map ((*) elementSize)
-        |> List.foldl (\id acc -> acc && roundTripAt id) True
+        |> List.foldl (\id acc -> roundTripAt id :: acc) []
+        |> List.unzip
+
+
+expectFloatListEqual : List Float -> List Float -> Expectation
+expectFloatListEqual l1 l2 =
+    let
+        expectFloatEqual x y =
+            if isNaN x && isNaN y then
+                Expect.pass
+            else
+                Expect.equal x y
+    in
+    case ( l1, l2 ) of
+        ( [], [] ) ->
+            Expect.pass
+
+        _ ->
+            Expect.all (List.map2 (\x y -> always (expectFloatEqual x y)) l1 l2) ()
 
 
 getterSetterRoundTrip : Test
@@ -108,33 +126,33 @@ getterSetterRoundTrip =
         [ fuzz TestFuzz.jsDataView "int8" <|
             \dataView ->
                 validGetterSetterRoundTrip 1 JsDataView.getInt8 MutableJsDataView.setInt8 dataView
-                    |> Expect.true "Round trip should have worked on all values"
+                    |> (\( l1, l2 ) -> Expect.equal l1 l2)
         , fuzz TestFuzz.jsDataView "uint8" <|
             \dataView ->
                 validGetterSetterRoundTrip 1 JsDataView.getUint8 MutableJsDataView.setUint8 dataView
-                    |> Expect.true "Round trip should have worked on all values"
+                    |> (\( l1, l2 ) -> Expect.equal l1 l2)
         , fuzz TestFuzz.jsDataView "int16" <|
             \dataView ->
                 validGetterSetterRoundTrip 2 JsDataView.getInt16 MutableJsDataView.setInt16 dataView
-                    |> Expect.true "Round trip should have worked on all values"
+                    |> (\( l1, l2 ) -> Expect.equal l1 l2)
         , fuzz TestFuzz.jsDataView "uint16" <|
             \dataView ->
                 validGetterSetterRoundTrip 2 JsDataView.getUint16 MutableJsDataView.setUint16 dataView
-                    |> Expect.true "Round trip should have worked on all values"
+                    |> (\( l1, l2 ) -> Expect.equal l1 l2)
         , fuzz TestFuzz.jsDataView "int32" <|
             \dataView ->
                 validGetterSetterRoundTrip 4 JsDataView.getInt32 MutableJsDataView.setInt32 dataView
-                    |> Expect.true "Round trip should have worked on all values"
+                    |> (\( l1, l2 ) -> Expect.equal l1 l2)
         , fuzz TestFuzz.jsDataView "uint32" <|
             \dataView ->
                 validGetterSetterRoundTrip 4 JsDataView.getUint32 MutableJsDataView.setUint32 dataView
-                    |> Expect.true "Round trip should have worked on all values"
+                    |> (\( l1, l2 ) -> Expect.equal l1 l2)
         , fuzz TestFuzz.jsDataView "float32" <|
             \dataView ->
                 validGetterSetterRoundTrip 4 JsDataView.getFloat32 MutableJsDataView.setFloat32 dataView
-                    |> Expect.true "Round trip should have worked on all values"
+                    |> (\( l1, l2 ) -> expectFloatListEqual l1 l2)
         , fuzz TestFuzz.jsDataView "float64" <|
             \dataView ->
                 validGetterSetterRoundTrip 8 JsDataView.getFloat64 MutableJsDataView.setFloat64 dataView
-                    |> Expect.true "Round trip should have worked on all values"
+                    |> (\( l1, l2 ) -> expectFloatListEqual l1 l2)
         ]
